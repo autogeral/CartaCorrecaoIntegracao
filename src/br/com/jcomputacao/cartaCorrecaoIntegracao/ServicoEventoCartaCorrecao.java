1ffe67805cf65e1024d201993d7b920a52a583fc
murilolima <murilolima@5adba155-f38a-43c0-aea0-d88c706dde45>
2018-08-14 13:24:39 +0000

    [update] mudando o endereco de envio da carta de correcao de acordo com novas especificacoes do Sefaz
    git-svn-id: https://svn.autogeral.com.br/code/CartaCorrecaoIntegracao@10577 5adba155-f38a-43c0-aea0-d88c706dde45

--- src/br/com/jcomputacao/cartaCorrecaoIntegracao/ServicoEventoCartaCorrecao.java
+++ src/br/com/jcomputacao/cartaCorrecaoIntegracao/ServicoEventoCartaCorrecao.java
@@ -4,8 +4,7 @@
  */
 package br.com.jcomputacao.cartaCorrecaoIntegracao;
 
-import br.com.jcomputacao.cartaCorrecao.ws.recepcaoEvento.RecepcaoEventoStub;
-import br.com.jcomputacao.cartaCorrecao.ws.recepcaoEvento.RecepcaoEventoStub.NfeRecepcaoEventoResult;
+import br.com.jcomputacao.cartaCorrecao.ws.recepcaoEvento4.NFeRecepcaoEvento4Stub;
 import java.rmi.RemoteException;
 import javax.xml.stream.XMLStreamException;
 import org.apache.axiom.om.OMElement;
@@ -19,19 +18,12 @@ import org.apache.axis2.AxisFault;
 public class ServicoEventoCartaCorrecao {
 
     public String executar(String xml) throws AxisFault, XMLStreamException, RemoteException {
-        RecepcaoEventoStub stub = new RecepcaoEventoStub();
-        RecepcaoEventoStub.NfeCabecMsg7 cabecalho = new RecepcaoEventoStub.NfeCabecMsg7();
-        RecepcaoEventoStub.NfeCabecMsg cabec = new RecepcaoEventoStub.NfeCabecMsg();
-
-        cabec.setCUF("35");
-        cabec.setVersaoDados("1.00");
-        cabecalho.setNfeCabecMsg(cabec);
-
-        RecepcaoEventoStub.NfeDadosMsg mensagem = new RecepcaoEventoStub.NfeDadosMsg();
+        NFeRecepcaoEvento4Stub stub = new NFeRecepcaoEvento4Stub();        
         OMElement element = AXIOMUtil.stringToOM(xml);
-        mensagem.setExtraElement(element);
-        
-        NfeRecepcaoEventoResult resultado = stub.nfeRecepcaoEvento(mensagem, cabecalho);
+        NFeRecepcaoEvento4Stub.NfeDadosMsg mensagem = new NFeRecepcaoEvento4Stub.NfeDadosMsg();        
+        mensagem.setExtraElement(element);        
+
+        NFeRecepcaoEvento4Stub.NfeResultMsg resultado = stub.nfeRecepcaoEvento(mensagem);
         element = resultado.getExtraElement();
         return element.toString();
     }
